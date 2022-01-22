359fdde6be79aecd3c6e45be8fb1066306d8e713
Marcus Kok <marcusk1699@gmail.com>
2022-01-22 18:17:07 -0500

    Fun stuff with cogs

--- src/nelson.py
+++ src/nelson.py
@@ -4,7 +4,10 @@ import random
 import discord
 from discord import user
 from discord.ext.commands.errors import CommandNotFound
+from discord.ext import tasks
 from dotenv import load_dotenv
+
+import reddit
 
 from discord.ext import commands
 
@@ -12,38 +15,45 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+# client = discord.Client()
+rh = reddit.RedditHandler("ncsu")
 
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+class FunStuff(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Nelson has connected to Discord!')
 
 # eight ball question event handler
-@bot.command(name='ask', help='Responds to yes or no question')
-async def eightball(ctx, question):
-    eightball_responses = [
-        'Yes',
-        'No',
-        'Definitely',
-        'Absolutely Not',
-        'Ask me later',
-    ]  
-    response = random.choice(eightball_responses)
-    await ctx.send(response)
+    @commands.command(name='ask', help='Responds to yes or no question')
+    async def eightball(self, ctx):
+        eightball_responses = [
+            'Yes',
+            'No',
+            'Definitely',
+            'Absolutely Not',
+            'Ask me later',
+        ]  
+        response = random.choice(eightball_responses)
+        await ctx.send(response)
 
-@eightball.error
-async def info_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('You need to ask a question first')
+    @eightball.error
+    async def info_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You need to ask a question first')
 
-@bot.command(name='roll_dice', help='Generates random number 1-6')
-async def roll(ctx):
-    await ctx.send(random.choice(range(1,6)))
+    @commands.command(name='roll_dice', help='Generates random number 1-6')
+    async def roll(self, ctx):
+        await ctx.send(random.choice(range(1,6)))
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send('Your command was not found :(')
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, CommandNotFound):
+            await ctx.send("I don't know that command :(")
 
-
+bot.add_cog(FunStuff(bot))
+# client.run(TOKEN)
 bot.run(TOKEN)
